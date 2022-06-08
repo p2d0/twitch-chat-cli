3239180b404e8cacfbf35511652d5bd29410bd13
Patriot720 <cerkin-3@yandex.ru>
2022-06-08 10:19:48 +0300

    Revert "remove badges emoticons && replaceEmotes"
    This reverts commit d012fccb2a966dd614d28b9955936724770a3601.

--- client.js
+++ client.js
@@ -37,7 +37,7 @@ const replaceEmotes = (words, emotes) => {
 
 const getStyledMessage = (message, emotes) => {
   let words = message.split(/\s+/)
-  const pipeline = [boldMentions]
+  const pipeline = [boldMentions, replaceEmotes]
   pipeline.forEach(pipe => {
     words = pipe(
       words,
@@ -48,9 +48,9 @@ const getStyledMessage = (message, emotes) => {
 }
 
 const getBadge = user => {
-  const modBadge = user.mod ? 'MOD ' : ''
-  const subBadge = user.subscriber ? 'SUB' : ''
-  const turboBadge = user.turbo ? 'TURBO' : ''
+  const modBadge = user.mod ? '⚔️' : ''
+  const subBadge = user.subscriber ? '🌟' : ''
+  const turboBadge = user.turbo ? '🔋' : ''
 
   return `${modBadge}${subBadge}${turboBadge}`
 }
